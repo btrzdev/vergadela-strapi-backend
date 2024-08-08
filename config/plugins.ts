ada222ae0f47470ed3fe9367c26838dc068d23b6
btrzdev <btrz.dev@gmail.com>
2024-08-08 11:03:57 +0100

    fix: config to deploy

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+const crypto = require('crypto');
+
+export default ({ env }) => ({
+  // ...
+  'users-permissions': {
+    config: {
+      jwt: {
+        jwtSecret: env('STRAPI_JWT_SECRET') || crypto.randomBytes(16).toString('base64')
+      },
+    },
+  },
+});
